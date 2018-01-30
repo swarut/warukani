a821ee589dec8cbc26bb13992358afa9c0e0eaab
Warut Surapat <s.warut@gmail.com>
2018-01-30 23:44:39 +0900

    Add dashboard.

---
+++ src/js/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton';
+import '../../css/dashboard.css'
+
+class Dashboard extends React.Component {
+  render () {
+    return (
+      <div className='dashboard'>
+        <RaisedButton label="Practice" className='button1' />
+        <RaisedButton label="Lookup" />
+      </div>
+    )
+  }
+
+}
+
+export default Dashboard
--- src/js/components/Root.js
+++ src/js/components/Root.js
@@ -7,6 +7,7 @@ import {
 import Home from './Home'
 import About from './About'
 import Loading from './Loading'
+import Dashboard from './Dashboard'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import waruTheme from '../warutheme.js';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -22,12 +23,14 @@ const Root = () => (
           <ul>
             <li><Link to="/"><FlatButton label="Home" primary={true} /></Link></li>
             <li><Link to="/loading"><FlatButton label="Loading" primary={true} /></Link></li>
+            <li><Link to="/dashboard"><FlatButton label="Dashboard" primary={true} /></Link></li>
             <li><Link to="/about"><FlatButton label="About" primary={true} /></Link></li>
           </ul>
         </div>
         <div className='body'>
           <Route exact path="/" component={Home} />
           <Route exact path="/loading" component={Loading} />
+          <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/about" component={About} />
         </div>
       </div>
