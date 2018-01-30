7ac56fa81c0d0f71c0fc587f638ba5ced7b2b257
Warut Surapat <s.warut@gmail.com>
2018-01-31 01:32:34 +0900

    Update ui.

--- src/js/components/Practice.js
+++ src/js/components/Practice.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import '../../css/practice.css'
-
+import TextField from 'material-ui/TextField';
 class Practice extends React.Component {
   render() {
     return (
       <div className='practice'>
-        practicse
+        <div className='practice-wraper'>
+          <div className='question'>
+            Advanced
+          </div>
+          <div className='answer'>
+            <TextField hintText='Your api token' floatingLabelText='Your api token'/>
+          </div>
+          <div className='solutions'>
+            <div className='item'>
+              <div className='word'>進展</div>
+            </div>
+            <div className='item'>
+              <div className='word'>進展</div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
