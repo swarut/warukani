372456317d4adf83d0b6da21e8095dda53e5f077
Warut Surapat <s.warut@gmail.com>
2018-02-02 23:53:55 +0900

    Add word item.

--- src/js/components/Practice.js
+++ src/js/components/Practice.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../../css/practice.css'
-import TextField from 'material-ui/TextField';
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+
 class Practice extends React.Component {
   render() {
     return (
@@ -11,13 +13,36 @@ class Practice extends React.Component {
           </div>
           <div className='answer'>
             <TextField hintText='Your api token' floatingLabelText='Your api token'/>
+            <RaisedButton label=">" />
           </div>
           <div className='solutions'>
             <div className='item'>
-              <div className='word'>進展</div>
+              <div className='word'>
+                <div className='word'>進展</div>
+                <div className='level'>4</div>
+              </div>
+              <div className='details'>
+                <div className='yomu'>しんてん</div>
+                <div className='meaning'>Advance</div>
+                <div className='tags'>
+                  <div className='tag'>evo1</div>
+                  <div className='tag'>evo2</div>
+                  <div className='tag'>evo3</div>
+                </div>
+              </div>
             </div>
             <div className='item'>
-              <div className='word'>進展</div>
+              <div className='word'>
+                <div className='word'>進展</div>
+                <div className='level'>4</div>
+              </div>
+              <div className='details'>
+                <div className='yomu'>しんてん</div>
+                <div className='meaning'>Advance</div>
+                <div className='tags'>
+                  <div className='tag'>evo</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
