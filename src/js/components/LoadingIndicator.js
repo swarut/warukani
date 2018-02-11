a3cf3dd54d4c809a893db15da6507192845b4579
Warut Surapat <s.warut@gmail.com>
2018-02-11 12:19:30 +0900

    Adjust loading indicator.

--- src/js/components/LoadingIndicator.js
+++ src/js/components/LoadingIndicator.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import '../../css/loading-indicator.css';
 
 class LoadingIndicator extends React.Component {
+
+  completion() {
+    return (this.props.currentProgress / this.props.totalProgress) * 100
+  }
+
   render() {
     return (
       <div className='loading-indicator'>
-        <div className='loading-progress' />
+        <div className='loading-progress' style={{width: this.completion() + '%'}} />
+        <div className='progress-number'>{this.completion()}</div>
       </div>
     )
   }
+}
+
+LoadingIndicator.propTypes = {
+  currentProgress: PropTypes.number.isRequired,
+  totalProgress: PropTypes.number.isRequired
+}
+
+LoadingIndicator.defaultProps = {
+  currentProgress: 40,
+  totalProgress: 100
 }
 
 export default LoadingIndicator
