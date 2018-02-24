60184b64836fdd200f1427d46ef64faffba1ebcd
Warut Surapat <s.warut@gmail.com>
2018-02-24 23:58:49 +0900

    Update setting.

--- src/js/components/Setting.js
+++ src/js/components/Setting.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 import '../../css/setting.css';
 
 class Setting extends React.Component {
+  constructor(props) {
+    super(props)
+    this.saveSetting = this.saveSetting.bind(this)
+  }
+  saveSetting() {
+    console.log("save setting")
+  }
   render() {
     return (
       <div className='setting'>
         <div className='setting-item'>
-          <label></label>
           <SelectField
-            floatingLabelText="Frequency"
+            floatingLabelText="Font"
             value={'sdfsd'}
             onChange={this.handleChange}>
             <MenuItem value={1} primaryText='font' />
+            <MenuItem value={2} primaryText='font' />
+            <MenuItem value={3} primaryText='font' />
+            <MenuItem value={4} primaryText='font' />
           </SelectField>
+        </div>
+        <div className='setting-item save'>
+          <RaisedButton label='Save' onClick={this.saveSetting} />
         </div>
       </div>
     )
