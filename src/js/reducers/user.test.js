9a14d768ce2103d35d258887566d092035c8bef7
Warut Surapat <s.warut@gmail.com>
2018-03-19 23:50:24 +0900

    Add user reducer test.

--- src/js/reducers/user.test.js
+++ src/js/reducers/user.test.js
@@ -1,16 +1,34 @@
 import user from './user'
 
 import {
-  authenticate
+  authenticate,
+  fetchUserInformation,
+  receivedUserInformation
 } from '../actions/actions'
 
-describe('actions', () => {
-  test('fetchVocabs returns the right action', () => {
+describe('user reducer', () => {
+  test('updates token and username for authenticate action', () => {
     let action = authenticate('token', 'username')
-    let userResult = user({}, action)
-    expect(userResult).not.toBeNull()
-    expect(userResult.token).toBe('token')
-    expect(userResult.username).toBe('username')
+    let updatedState = user({}, action)
+    expect(updatedState).not.toBeNull()
+    expect(updatedState.token).toBe('token')
+    expect(updatedState.username).toBe('username')
+  })
+
+  test('updates fetching status for fetch user information action', () => {
+    let action = fetchUserInformation()
+    let updatedState = user({}, action)
+    expect(updatedState).not.toBeNull()
+    expect(updatedState.isFetching).toBe(true)
+  })
+
+  test('updates user information for received user information action', () => {
+    let action = receivedUserInformation('info')
+    let updatedState = user({}, action)
+    expect(updatedState).not.toBeNull()
+    expect(updatedState.userInformation).toBe('info')
+    expect(updatedState.isFetching).toBe(false)
+    expect(updatedState.wasFetched).toBe(true)
   })
 
 })
