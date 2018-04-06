c4e68a496bdb6c3831db348ce8a4f1a260df1e8e
Warut Surapat <s.warut@gmail.com>
2018-04-06 23:51:48 +0900

    Add index for 7 chars.

--- src/js/actions/vocabActions.js
+++ src/js/actions/vocabActions.js
@@ -74,7 +74,8 @@ export const fetchVocabsOfAllLevels = (token, numberOfLevel) => {
           let key2 = res.meaning.replace(' ', '').slice(0, 4)
           let key3 = res.meaning.replace(' ', '').slice(0, 5)
           let key4 = res.meaning.replace(' ', '').slice(0, 6)
-          let keys = [key1, key2, key3, key4]
+          let key5 = res.meaning.replace(' ', '').slice(0, 7)
+          let keys = [key1, key2, key3, key4, key5]
           keys.forEach((key) => {
             if(!lookUp[key]) {
               lookUp[key] = [idCounter]
