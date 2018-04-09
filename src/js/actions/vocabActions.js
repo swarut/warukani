5eb13c9904b3f0ac62f2cd19768d49b801598f52
Warut Surapat <s.warut@gmail.com>
2018-04-09 23:49:21 +0900

    I really got the emergency.

--- src/js/actions/vocabActions.js
+++ src/js/actions/vocabActions.js
@@ -76,6 +76,7 @@ export const fetchVocabsOfAllLevels = (token, numberOfLevel) => {
           let key4 = res.meaning.replace(' ', '').slice(0, 6)
           let keys = [key1, key2, key3, key4]
           // I'm so lazy today.
+          // and I had an emergency issue today.
           keys.forEach((key) => {
             if(!lookUp[key]) {
               lookUp[key] = [idCounter]
